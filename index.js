b971332426f7dba6ded67110c010bd26a0bba5fd
Dima34 <vavilov9823@gmail.com>
2022-07-24 00:29:46 +0300

    endgame added, 'start typing indicatoa added'

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const textList = [
-    `The color of animals is by no means a matter of chance; it depends on many considerations, but in the majority of cases tends to protect the animal from danger by rendering it less conspicuous. Perhaps it may be said that if coloring is mainly protective, there ought to be but few brightly colored animals. There are, however, not a few cases in which vivid colors are themselves protective. The kingfisher itself, though so brightly colored, is by no means easy to see. The blue harmonizes with the water, and the bird as it darts along the stream looks almost like a flash of sunlight. Desert animals are generally the color of the desert. Thus, for instance, the lion, the antelope, and the wild donkey are all sand-colored. “Indeed,” says Canon Tristram, “in the desert, where neither trees, brushwood, nor even undulation of the surface afford the slightest protection to its foes, a modification of color assimilated to that of the surrounding country is absolutely necessary. Hence, without exception, the upper plumage of every bird, and also the fur of all the smaller mammals and the skin of all the snakes and lizards, is of one uniform sand color.” The next point is the color of the mature caterpillars, some of which are brown. This probably makes the caterpillar even more conspicuous among the green leaves than would otherwise be the case. Let us see, then, whether the habits of the insect will throw any light upon the riddle. What would you do if you were a big caterpillar? Why, like most other defenseless creatures, you would feed by night, and lie concealed by day. So do these caterpillars. When the morning light comes, they creep down the stem of the food plant, and lie concealed among the thick herbage and dry sticks and leaves, near the ground, and it is obvious that under such circumstances the brown color really becomes a protection. It might indeed be argued that the caterpillars, having become brown, concealed themselves on the ground, and that we were reversing the state of things. But this is not so, because, while we may say as a general rule that large caterpillars feed by night and lie concealed by day, it is by no means always the case that they are brown; some of them still retaining the green color. We may then conclude that the habit of concealing themselves by day came first, and that the brown color is a later adaptation.`
+    `The color of animals is by no means a matter of chance; it depends on many considerations, but in the majority of cases tends to protect the animal from danger by rendering it less conspicuous. Perhaps it may be said that if coloring is mainly protective, there ought to be but few brightly colored animals. There are, however, not a few cases in which vivid colors are themselves protective. The kingfisher itself, though so brightly colored, is by no means easy to see. The blue harmonizes with the water, and the bird as it darts along the stream looks almost like a flash of sunlight. Desert animals are generally the color of the desert. Thus, for instance, the lion, the antelope, and the wild donkey are all sand-colored. “Indeed,” says Canon Tristram, “in the desert, where neither trees, brushwood, nor even undulation of the surface afford the slightest protection to its foes, a modification of color assimilated to that of the surrounding country is absolutely necessary. Hence, without exception, the upper plumage of every bird, and also the fur of all the smaller mammals and the skin of all the snakes and lizards, is of one uniform sand color.” The next point is the color of the mature caterpillars, some of which are brown. This probably makes the caterpillar even more conspicuous among the green leaves than would otherwise be the case. Let us see, then, whether the habits of the insect will throw any light upon the riddle. What would you do if you were a big caterpillar? Why, like most other defenseless creatures, you would feed by night, and lie concealed by day. So do these caterpillars. When the morning light comes, they creep down the stem of the food plant, and lie concealed among the thick herbage and dry sticks and leaves, near the ground, and it is obvious that under such circumstances the brown color really becomes a protection. It might indeed be argued that the caterpillars, having become brown, concealed themselves on the ground, and that we were reversing the state of things. But this is not so, because, while we may say as a general rule that large caterpillars feed by night and lie concealed by day, it is by no means always the case that they are brown; some of them still retaining the green color. We may then conclude that the habit of concealing themselves by day came first, and that the brown color is a later adaptation.`,
+    `The self-study lessons in this section are written and organised according to the levels of the Common European Framework of Reference for languages (CEFR). There are different types of texts and interactive exercises that practise the reading skills you need to do well in your studies, to get ahead at work and to communicate in English in your free time.`,
+    `Reading practice to help you understand texts with a wide vocabulary where you may need to consider the writer's opinion. Texts include articles, reports, messages, short stories and reviews.`,
+    "It was long, long ago when the Fox and the Crane were close friends. One fine day the Fox invited the Crane to dinner with her and said to him: «Come, buddy! Come, my dear! I’ll treat you heartily!” And so the Crane came to the Fox for the dinner party. The Fox had cooked semolina for the dinner and smeared it over the plate. Then she served it and treated her guest.",
+    `When we talk about motivating others, the justification is the end result (either we want to avoid the pain or go towards pleasure) or what we want to get the person to do. How we achieve the end result, are our alternatives. As a manager, we need to understand the other person's justification and then come up with alternatives. We may then choose the right alternative. However, in general, we choose the first or the emotionally satisfying one. Typically people stop at this level of analysis and start to act. But a good manager would think of the following also: Will the action guarantee the consequence? What about other unintended consequences? This requires a certain experience. Are we capable of doing this action? Intention and the selection of the most ideal alternative do not guarantee execution, if we do not have the skills and the experience. Most motivational tactics fail, because without execution capability, they is only wishful thinking.`,
+    `Income before securities transactions was up 10.8 percent from $13.49 million in 1982 to $14.95 million in 1983. Earnings per share (adjusted for a 10.5 percent stock dividend distributed on August 26) advanced 10 percent to $2.39 in 1983 from $2.17 in 1982. Earnings may rise for 7 years. Hopefully, earnings per share will grow another 10 percent. Kosy, Klemin, and Bille began selling on May 23, 1964. Their second store was founded in Renton on August 3, 1965. From 1964 to 1984, they opened more than 50 stores through-out the country. As they expanded, 12 regional offices had to be organized. Each of these 12 regional offices had to be organized. Each of these 12 regions employs from 108 to 578 people. National headquarters employs 1,077 people. Carole owns 118 stores located in 75 cities ranging as far west as Seattle and as far east as Boston. She owns 46 stores south of the Mason-Dixon line and 24 stores north of Denver. Carole buys goods from 89 companies located in 123 countries and all 50 states. Carole started in business on March 3, 1975. She had less than $6,000 in capital assets.`,
+    `Because of the laboriousness of the translation process, since the 1940s efforts have been made, with varying degrees of success, to automate translation or to mechanically aid the human translator. More recently, the rise of the Internet has fostered a world-wide market for translation services and has facilitated "language localization". Ideally, the translator must know both languages, as well as the subject that is to be translated.`,
+    `A freelancer or freelance worker, is a term commonly used for a person who is self-employed and is not necessarily committed to a particular employer long-term. Freelance workers are sometimes represented by a company or a temporary agency that resells freelance labor to clients; others work independently or use professional associations or websites to get work. While the term "independent contractor" would be used in a higher register of English to designate the tax and employment classes of this type of worker, the term freelancing is most common in culture and creative industries and this term specifically motions to participation therein. Fields, professions, and industries where freelancing is predominant include: music, writing, acting, computer programming, web design, graphic design, translating and illustrating, film and video production and other forms of piece work which some cultural theorists consider as central to the cognitive-cultural economy.`,
+    `Two common terms used to describe a salesperson are "Farmer" and "Hunter". The reality is that most professional salespeople have a little of both. A hunter is often associated with aggressive personalities who use aggressive sales technique. In terms of sales methodology, a hunter refers to a person whose focus is on bringing in and closing deals. This process is called "sales capturing". An example is a commodity sale such as a long distance salesperson, shoe salesperson and to a degree a car salesperson. Their job is to find and convert buyers. A sales farmer is someone who creates sales demand through activities that directly influence and alter the buying process.`,
+    `Trying to make a wise, good choice when thinking about what kinds of careers might be best for you is a hard thing to do. Your future may very well depend on the ways you go about finding the best job openings for you in the world of work. Some people will feel that there is one and only one job in the world for them. Hard thinking and a lot of hard work will help them find the one job that is best for them. Jobs are there for those with skills and a good work ethic. Many new young artists in the upper New England states are famous around the world as leaders in new American art. These fine artists are very good in their chosen fields and are willing to share their many talents by teaching others. The students have had the chance to learn and use skills in oil painting, sketching with chalk, sculpting, and weaving. Learning to typewrite is a skill that will help all of us in our work today. The development of the computer will open doors for people with the keyboarding skills and will make typing a necessity. Managers, as well as secretaries, will need skill at the keyboard to input data and process words. Therefore, good keyboarding skills may be important to you.`,
+    "This sort of character is met with pretty frequently in a certain class. They are people who know everyone—that is, they know where a man is employed, what his salary is, whom he knows, whom he married, what money his wife had, who are his cousins, and second cousins, etc., etc. These men generally have about a hundred pounds a year to live on, and they spend their whole time and talents in the amassing of this style of knowledge, which they reduce—or raise—to the standard of a science.",
+    "Towards the end of November, during a thaw, at nine o’clock one morning, a train on the Warsaw and Petersburg railway was approaching the latter city at full speed. The morning was so damp and misty that it was only with great difficulty that the day succeeded in breaking; and it was impossible to distinguish anything more than a few yards away from the carriage windows. Some of the passengers by this particular train were returning from abroad; but the third-class carriages were the best filled, chiefly with insignificant persons of various occupations and degrees, picked up at the different stations nearer town. All of them seemed weary, and most of them had sleepy eyes and a shivering expression, while their complexions generally appeared to have taken on the colour of the fog outside.",
+    "One of them was a young fellow of about twentyseven, not tall, with black curling hair, and small, grey, fiery eyes. His nose was broad and flat, and he had high cheek bones; his thin lips were constantly compressed into an impudent, ironical—it might almost be called a malicious—smile; but his forehead was high and well formed, and atoned for a good deal of the ugliness of the lower part of his face. A special feature of this physiognomy was its death-like pallor, which gave to the whole man an indescribably emaciated appearance in spite of his hard look, and at the same time a sort of passionate and suffering expression which did not harmonize with his impudent, sarcastic smile and keen, self-satisfied bearing. He wore a large fur—or rather astrachan—overcoat, which had kept him warm all night, while his neighbour had been obliged to bear the full severity of a Russian November night entirely unprepared. His wide sleeveless mantle with a large cape to it—the sort of cloak one sees upon travellers during the winter months in Switzerland or North Italy—was by no means adapted to the long cold journey through Russia, from Eydkuhnen to St. Petersburg.", 
+];
+
+
+const endingsList = [
+    {
+    
+    }
 ]
 
 const typedTextZone = document.getElementById("typedText");
@@ -10,27 +29,39 @@ const secondsRemainCounter = document.getElementById("secondsRemainCounter");
 const wordsMinCounter = document.getElementById("wordsMinCounter");
 const charsMinCounter = document.getElementById("charsMinCounter");
 const accuracyCounter = document.getElementById("accuracyCounter");
+const endPopup = document.getElementById("endPopup");
+const restartButton = document.getElementById("newGame");
+const typingIndicator = document.getElementById("startTypingIndicator");
 
 coursor.classList.add("deactivate");
 
-let typed = "";
-let needToType = "";
+let typed;
+let needToType;
 
-let secondsRemain = 60;
-let wrongChars = 0;
-let correctChars = 0;
-let wordsCount = 0;
+let secondsRemain = 10;
+let wrongChars;
+let correctChars;
+let wordsCount;
+
+let wpm;
+let cpm;
+let accuracy;
 
 let intervalId;
 let isStarted = false;
 let secondsFromStart = 0;
-let secondsToEnd;
+let secondsToEnd = 0;
+
 
 function Init() {
-    needToType = textList[0];
+    SetDefaultValues();
+    SetRendomText();
     VisualizeText();
+    VisualizeCounters();
     StartTypingSequence();
     CoursorCheckingSequence();
+    HandleEndPopupControlls();
+    ShowStartTypingIndicator();
 }
 
 function Destroy() {
@@ -38,21 +69,45 @@ function Destroy() {
     clearInterval(intervalId);
 }
 
+// Sets the default cunter values
+function SetDefaultValues() {
+    isStarted = false;
+
+    typed = "";
+    needToType = "";
+
+    secondsToEnd = secondsRemain;
+    wrongChars = 0;
+    correctChars = 0;
+    wordsCount = 0;
+}
+
+function SetRendomText() {
+    let randomTextNumber = Math.round(Math.random() * (textList.length - 1));
+    needToType = textList[randomTextNumber];
+}
+
 function TickTime() {
     console.log("tick");
     secondsToEnd--;
     secondsFromStart++;
-    VisualizeCounters();
 
-    if(secondsRemain == 0){
-        console.log("ho time");
+    VisualizeCounters();
+    CheckForEnd();
+}
+
+function CheckForEnd() {
+    if(secondsToEnd == 0){
         isStarted = false;
+        inputZone.blur();
+        OpenPopup();
         Destroy();
     }
 }
 
 function CheckForStart() {
     if(isStarted == false){
+        HideStartTypingIndicator();
         isStarted = true;
         secondsToEnd = secondsRemain;
         intervalId = setInterval(TickTime, 1000);
@@ -102,9 +157,9 @@ function GetNextCharCode(){
 function VisualizeCounters() {
     secondsRemainCounter.innerText = secondsToEnd;
 
-    let wpm = GetUnitPerMin(wordsCount, secondsFromStart);
-    let cpm = GetUnitPerMin(correctChars, secondsFromStart);
-    let accuracy = Math.round(100 -(wrongChars * 100 / correctChars))
+    wpm = GetUnitPerMin(wordsCount, secondsFromStart);
+    cpm = GetUnitPerMin(correctChars, secondsFromStart);
+    accuracy = Math.round(100 -(wrongChars * 100 / correctChars))
 
     wordsMinCounter.innerText = GetCorrectNum(wpm);
     charsMinCounter.innerText = GetCorrectNum(cpm);
@@ -180,5 +235,27 @@ function CoursorCheckingSequence() {
     })
 }
 
+function HandleEndPopupControlls(){
+    restartButton.addEventListener("click", ()=>{
+        ClosePopup();
+        Init();
+    })
+}
+
+function OpenPopup() {
+    endPopup.classList.add("active");
+}
+
+function ClosePopup() {
+    endPopup.className = "";
+}
+
+function ShowStartTypingIndicator(){
+    typingIndicator.classList.add("active");
+}
+
+function HideStartTypingIndicator(){
+    typingIndicator.classList.remove("active");
+}
 
 Init();
